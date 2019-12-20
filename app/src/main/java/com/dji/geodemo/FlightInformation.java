3f501119413c3935fa98cfc727119f915904b435
RoseNeverFade <1045553189@qq.com>
2019-12-20 20:27:53 +0800

    提交信息

---
+++ app/src/main/java/com/dji/geodemo/FlightInformation.java
@@ -0,0 +1,13 @@
+package com.dji.geodemo;
+
+import java.util.ArrayList;
+
+public class FlightInformation {
+    public static ArrayList<Double> demLat = new ArrayList<Double>();
+    public static ArrayList<Double> demLng = new ArrayList<Double>();
+    public static ArrayList<Double> demAltitude = new ArrayList<Double>();
+    public static ArrayList<Double> demPitch = new ArrayList<Double>();
+    public static ArrayList<Double> demRoll = new ArrayList<Double>();
+    public static ArrayList<Double> demYaw = new ArrayList<Double>();
+    public static ArrayList<String> demTime = new ArrayList<String>();
+}
